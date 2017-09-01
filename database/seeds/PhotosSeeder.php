6e83e6b78e938157ab56565728c7c2bfca5fd39d
Dejan Stosic <dejanstosic7@gmail.com>
2017-09-01 19:18:02 +0000

    seed

--- database/seeds/PhotosSeeder.php
+++ database/seeds/PhotosSeeder.php
@@ -11,6 +11,36 @@ class PhotosSeeder extends Seeder
      */
     public function run()
     {
-        //
+        $longitude = (float) 21.892018;
+        $latitude = (float) 43.318496;
+        $radius = rand(1,8); // in miles
+
+        $lng_min = $longitude - $radius / abs(cos(deg2rad($latitude)) * 69);
+        $lng_max = $longitude + $radius / abs(cos(deg2rad($latitude)) * 69);
+        $lat_min = $latitude - ($radius / 69);
+        $lat_max = $latitude + ($radius / 69);
+
+//        echo 'lng (min/max): ' . $lng_min . '/' . $lng_max . PHP_EOL;
+//        echo 'lat (min/max): ' . $lat_min . '/' . $lat_max;
+
+        for ($i =0 ; $i<50;$i++) {
+            $lng = $this->float_rand($lng_min, $lng_max);
+            $lat = $this->float_rand($lat_min, $lat_max);
+            $date = date("Y-m-d H:i:s");
+
+            $string = "INSERT INTO `photos` (user_id, filename, location, description, categories, created_at, updated_at) values (?, ?, POINT($lng,$lat), ?, ?, ?, ?)";
+            //(1, 'test.jpg', POINT($lng,$lat)), 'demo photo', '#nature', $date, $date)
+            DB::insert($string, [1, "test.jpg" , "demo photo", "#nature", $date, $date]);
+        }
+
+    }
+
+    public function float_rand($Min, $Max, $round=0){
+        //validate input
+        $randomfloat = $Min + mt_rand() / mt_getrandmax() * ($Max - $Min);
+        if($round > 0)
+            $randomfloat = round($randomfloat,$round);
+
+        return (float) substr($randomfloat, 0, 9);
     }
 }
